45b095f35aa56de6d0233960b6169edd9c05b5f0
Chamil Fonseka <fonseka.chamil@gmail.com>
2023-08-22 09:24:30 +0530

    configure and expose usefull management endpoints

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "3.1.2"
 	id("io.spring.dependency-management") version "1.1.2"
+	id("com.gorylenko.gradle-git-properties") version "2.4.1"
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
 	kotlin("plugin.jpa") version "1.8.22"
@@ -19,10 +20,20 @@ repositories {
 	mavenCentral()
 }
 
+springBoot {
+	buildInfo() {
+		properties {
+			name = "Course Catalog Service"
+			additional = mapOf("customKey1" to "customValue1", "customKey2" to "customValue2")
+		}
+	}
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("com.h2database:h2")
